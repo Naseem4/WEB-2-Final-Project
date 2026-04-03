8a6f7b76871f185d4ed0c4d83851a2703ba47543
Adam Fatayer <fatayemadam@gmail.com>
2026-04-03 03:59:13 +0300

    Adding the JS for both Sign in & Sign Up Page (Remaining 2 things to code )

---
+++ FrontEnd/sign_in/sign_in.js
@@ -0,0 +1,9 @@
+document.querySelector(".signUP").addEventListener("click", function() {
+  window.location.href = "../sign_up/sign_up.html";
+});
+
+/*
+remaining : 
+1. When click on the logo , go to the main page 
+2. Firebase google
+*/
---
+++ FrontEnd/sign_up/sign_up.js
@@ -0,0 +1,9 @@
+document.querySelector(".signIN").addEventListener("click", function() {
+  window.location.href = "../sign_in/sign_in.html";
+});
+
+/*
+remaining : 
+1. When click on the logo , go to the main page 
+2. Firebase google
+*/
