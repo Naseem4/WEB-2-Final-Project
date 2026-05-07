2f7c6dd6a2a8963b33057d7dc904f13852d18da0
Adam Fatayer <fatayemadam@gmail.com>
2026-05-07 10:43:19 +0300

    Adding React + Firebase for Sign in & Sign Up Pages

--- FrontEnd/sign_in/sign_in.js
+++
@@ -1,9 +0,0 @@
-document.querySelector(".signUP").addEventListener("click", function() {
-  window.location.href = "../sign_up/sign_up.html";
-});
-
-/*
-remaining : 
-1. When click on the logo , go to the main page 
-2. Firebase google
-*/
--- FrontEnd/sign_up/sign_up.js
+++
@@ -1,9 +0,0 @@
-document.querySelector(".signIN").addEventListener("click", function() {
-  window.location.href = "../sign_in/sign_in.html";
-});
-
-/*
-remaining : 
-1. When click on the logo , go to the main page 
-2. Firebase google
-*/
